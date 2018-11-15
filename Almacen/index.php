5355a7eb06937a318560b73d672d62ea11437506
pablo torres searle <pablots89@gmail.com>
2018-11-15 17:02:26 +0100

    Importados el boostrap y jquery

--- Almacen/index.php
+++ Almacen/index.php
@@ -6,8 +6,11 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+        <title>ALMACEN UFV</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+         <link href="css/miCss.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <?php
